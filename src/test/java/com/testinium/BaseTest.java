1adab96389bbf75405566e6e4767af8a58c5e7e6
hatice ozturk <haticeozturk0207@gmail.com>
2024-09-23 20:36:21 +0300

    Update BaseTest.java

--- src/test/java/com/testinium/BaseTest.java
+++ src/test/java/com/testinium/BaseTest.java
@@ -98,8 +98,8 @@ public class BaseTest {
                     capabilities.setCapability("key", properties.getProperty("key"));
                     browserName = System.getenv("browser");
                     //driver = new RemoteWebDriver(new URL("https://hubclouddev.testinium.com/wd/hub"), capabilities);
-                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-                    //driver = new RemoteWebDriver(new URL("http://host.docker.internal:4444/wd/hub"), capabilities);
+                    //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+                    driver = new RemoteWebDriver(new URL("http://host.docker.internal:4444/wd/hub"), capabilities);
 
                     actions = new Actions(driver);
                 }
